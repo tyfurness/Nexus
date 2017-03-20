d47bf127924874919a66d77ce3935392fff20ed6
Unknown <tyfurness@gmail.com>
2017-03-20 11:13:34 -0400

    Adding an option to check for available ads.

--- Nexus/Assets/Source/Managers/Nexus.cs
+++ Nexus/Assets/Source/Managers/Nexus.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Advertisements;
-using UnityEngine.Analytics;
-using UnityEngine.Purchasing;
+﻿using UnityEngine;
 
 /// <summary>
 /// Manager that holds references to the different managers
